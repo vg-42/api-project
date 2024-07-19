684c459e6cdbbf69949ce764f617c2f581ec66d7
Pratik <123054542+PratikKharadGophygital@users.noreply.github.com>
2024-07-19 15:36:47 +0530

    Update ErrorViewModel.cs

--- MyApiProjectOAutho/Models/ErrorViewModel.cs
+++ MyApiProjectOAutho/Models/ErrorViewModel.cs
@@ -3,7 +3,7 @@ namespace MyApiProjectOAutho.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
+        public string? RequestIdOne { get; set; }
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
