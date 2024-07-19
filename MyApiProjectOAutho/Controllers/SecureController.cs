4c292833922e12d455b2da87e36d55b54cfef95f
Pratik <pratik.kharad@gophygital.io>
2024-07-19 12:15:52 +0530

    Add project files.

---
+++ MyApiProjectOAutho/Controllers/SecureController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApiProjectOAutho.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class SecureController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("You have accessed secured API endpoint.");
+        }
+    }
+}
---
+++ MyApiProjectOAutho/Controllers/TokenController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApiProjectOAutho.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TokenController : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult GetToken()
+        {
+            var accessToken = HttpContext.GetTokenAsync("access_token").Result;
+            return Ok(new { access_token = accessToken });
+        }
+    }
+}
+
